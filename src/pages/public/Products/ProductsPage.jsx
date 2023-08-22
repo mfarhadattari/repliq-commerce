16ad3ad96c6dcdc18b480f1abfa5c2fe23de098c
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-22 16:19:01 +0600

    fetch all product on products page using useFetchData hooks and manitain loading state

--- src/pages/public/Products/ProductsPage.jsx
+++ src/pages/public/Products/ProductsPage.jsx
@@ -1,12 +1,19 @@
 import banner from "../../../assets/images/banner.jpg";
+import Loaders from "../../../components/common/Loaders";
 import MenuCover from "../../../components/common/MenuCover";
 import PageTitle from "../../../components/common/PageTitle";
+import useFetchData from "../../../hooks/useFetchData";
 
 const ProductsPage = () => {
+  const { data: products, isLoading: isProductLoading } = useFetchData(
+    "/products",
+    []
+  );
   return (
     <div>
       <PageTitle title="Products" />
       <MenuCover backgroundURL={banner} heading="Our Products" />
+      {isProductLoading ? <Loaders /> : <div>{products.length}</div>}
     </div>
   );
 };
