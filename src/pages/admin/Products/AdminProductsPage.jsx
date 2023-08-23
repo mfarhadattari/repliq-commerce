bd25816a067f9e8669274a1226cf204e8b530d7a
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-23 20:42:53 +0600

    load product data in AdminProductPage

--- src/pages/admin/Products/AdminProductsPage.jsx
+++ src/pages/admin/Products/AdminProductsPage.jsx
@@ -1,10 +1,22 @@
+import Loaders from "../../../components/common/Loaders";
 import PageTitle from "../../../components/common/PageTitle";
+import useFetchData from "../../../hooks/useFetchData";
 
 const AdminProductsPage = () => {
+  const { data: products, isLoading: isProductLoading } =
+    useFetchData("/admin/products");
+
+  console.log(products, isProductLoading);
   return (
     <main>
       <PageTitle title="Products" />
-      <h1>Products Page</h1>
+      <section className="my-10  mx-auto">
+        {isProductLoading || !products ? (
+          <Loaders />
+        ) : (
+          <div>{products.length}</div>
+        )}
+      </section>
     </main>
   );
 };
