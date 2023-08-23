c4caa376f10694fd3c3cda0e0dbab3f22f6481d9
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-23 10:22:38 +0600

    Order data load

--- src/pages/user/OrdersPage.jsx
+++ src/pages/user/OrdersPage.jsx
@@ -1,12 +1,28 @@
+import Loaders from "../../components/common/Loaders";
 import PageTitle from "../../components/common/PageTitle";
+import SectionTitle from "../../components/common/SectionTitle";
+import useAuth from "../../hooks/useAuth";
+import useFetchData from "../../hooks/useFetchData";
 
 const OrdersPage = () => {
-  return (
-    <div>
-      <PageTitle title="Orders" />
+  const { authUser } = useAuth();
 
-      <h1>Orders Page</h1>
-    </div>
+  // load cart data
+  const { data: orders, isLoading: isOrderLoading } = useFetchData(
+    `/my-orders?userPhone=${authUser?.userPhone}`,
+    [],
+    [authUser]
+  );
+
+  console.log(orders);
+  return (
+    <main className="my-10">
+      <PageTitle title="Orders" />
+      <section>
+        <SectionTitle heading="My Orders!" />
+        {isOrderLoading || !orders ? <Loaders /> : <div>{orders.length}</div>}
+      </section>
+    </main>
   );
 };
 
