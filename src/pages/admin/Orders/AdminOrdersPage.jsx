b0ab72f62c8c11c5e0c459b7c4a99b42c8e4fe3b
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-24 11:54:30 +0600

    admin all orders data load

--- src/pages/admin/Orders/AdminOrdersPage.jsx
+++ src/pages/admin/Orders/AdminOrdersPage.jsx
@@ -1,10 +1,23 @@
+import Loaders from "../../../components/common/Loaders";
 import PageTitle from "../../../components/common/PageTitle";
+import SectionTitle from "../../../components/common/SectionTitle";
+import useFetchData from "../../../hooks/useFetchData";
 
 const AdminOrdersPage = () => {
+  const { data: orders, isLoading: isOrderLoading } = useFetchData(
+    "/admin/orders",
+    [],
+    ["orders"]
+  );
+
+  console.log(orders, isOrderLoading);
   return (
     <main>
-      <PageTitle title="" />
-      Orders Page
+      <PageTitle title="Order Page" />
+      <section>
+        <SectionTitle heading="ALL ORDERS" />
+        {isOrderLoading || !orders ? <Loaders /> : <div>Orders</div>}
+      </section>
     </main>
   );
 };
