bfc10907facd93755a7ebcd2f33e5b52a58d9b7c
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-24 11:26:33 +0600

    AdminData Load from servevr

--- src/pages/admin/AdminHome/AdminHomePage.jsx
+++ src/pages/admin/AdminHome/AdminHomePage.jsx
@@ -1,10 +1,19 @@
+import Loaders from "../../../components/common/Loaders";
 import PageTitle from "../../../components/common/PageTitle";
+import useFetchData from "../../../hooks/useFetchData";
 
 const AdminHomePage = () => {
+  const { data: adminData, isLoading: isAdminDataLoading } = useFetchData(
+    "/admin/",
+    {},
+    ["adminData"]
+  );
+
+  console.log(adminData, isAdminDataLoading);
   return (
     <main>
       <PageTitle title="Admin Home" />
-      Admin Home Page
+      {isAdminDataLoading || !adminData ? <Loaders /> : <div></div>}
     </main>
   );
 };
