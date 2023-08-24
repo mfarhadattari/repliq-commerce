b608dbda7dde7dda8c9eb4131f178d4a9b01b7ff
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-24 12:48:12 +0600

    order data load from server

--- src/pages/admin/Orders/OrdersDetailsPage.jsx
+++ src/pages/admin/Orders/OrdersDetailsPage.jsx
@@ -1,10 +1,23 @@
+import { useParams } from "react-router-dom";
+import Loaders from "../../../components/common/Loaders";
 import PageTitle from "../../../components/common/PageTitle";
+import SectionTitle from "../../../components/common/SectionTitle";
+import useFetchData from "../../../hooks/useFetchData";
 
 const OrdersDetailsPage = () => {
+  const { id } = useParams();
+  const { data: order, isLoading } = useFetchData(`admin/orders/${id}`, {}, [
+    id,
+  ]);
+
+  console.log(order, isLoading);
   return (
     <main>
       <PageTitle title="Order Details" />
-      Orders Details Page
+      <section>
+        <SectionTitle heading="Order Details" />
+        {isLoading || !order ? <Loaders /> : <div>Order Details</div>}
+      </section>
     </main>
   );
 };
