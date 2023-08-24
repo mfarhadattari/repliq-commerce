e31d0f0e5d67c5e084ebc588b4385a7444701f83
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-24 11:01:11 +0600

    load customer details data

--- src/pages/admin/Customers/CustomerDetailsPage.jsx
+++ src/pages/admin/Customers/CustomerDetailsPage.jsx
@@ -1,10 +1,26 @@
+import { useParams } from "react-router-dom";
+import Loaders from "../../../components/common/Loaders";
 import PageTitle from "../../../components/common/PageTitle";
+import useFetchData from "../../../hooks/useFetchData";
 
 const CustomerDetailsPage = () => {
+  const { id } = useParams();
+  const { data: customer, isLoading: isCustomerLoading } = useFetchData(
+    `/admin/customers/${id}`,
+    {},
+    [id]
+  );
+
   return (
     <main>
       <PageTitle title="Customer Details" />
-      Customer Details Page
+      <section>
+        {isCustomerLoading || !customer ? (
+          <Loaders />
+        ) : (
+          <div>Customer Details</div>
+        )}
+      </section>
     </main>
   );
 };
